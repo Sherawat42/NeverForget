eb28361e3593495f2aa19279361a5ca9e0b0c012
sherawat42 <manishsherawat1@gmail.com>
2017-02-13 01:06:06 +0530

    half done

--- app/src/main/java/com/awesome/sherawat42/neverforget/MainActivity.java
+++ app/src/main/java/com/awesome/sherawat42/neverforget/MainActivity.java
@@ -1,13 +1,29 @@
 package com.awesome.sherawat42.neverforget;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+
+
+import java.util.ArrayList;
+
+// TODO (1) Add Support for change of time zone!!
 
 public class MainActivity extends AppCompatActivity {
 
+    ArrayList<Todo> todoList;
+    Todo testTodo = new Todo("testing todo message");
+    TodoArrayAdapter todoArrayAdapter;
+    ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        todoList = new ArrayList<>();
+        for(int i=0;i<20;i++){
+            todoList.add(testTodo);
+        }
+        listView = (ListView)findViewById(R.id.todoListView);
+        listView.setAdapter(todoArrayAdapter);
     }
 }
