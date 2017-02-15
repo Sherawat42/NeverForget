c90a54758aa7a6febea1f67a64513b887dd9639f
sherawat42 <manishsherawat1@gmail.com>
2017-02-15 19:18:20 +0530

    commit

--- app/src/main/java/com/awesome/sherawat42/neverforget/MainActivity.java
+++ app/src/main/java/com/awesome/sherawat42/neverforget/MainActivity.java
@@ -2,6 +2,8 @@ package com.awesome.sherawat42.neverforget;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ListView;
 
 
@@ -31,5 +33,21 @@ public class MainActivity extends AppCompatActivity {
 ////        adapter = new ArrayAdapter<>(this,R.layout.list_item,R.id.listItemTextView,expenses);
 //        adapter = new ExpenseAdapter(this,expenses);
 //        listView.setAdapter(adapter);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.add){
+            
+        }
+        return true;
     }
 }
